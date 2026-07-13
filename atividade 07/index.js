445710706f911ef8f0f9d58e4907610149749766
Lucas Marques <lucasmarques180506@gmail.com>
2026-07-13 16:06:16 -0300

    feat: anotations

--- atividade 07/index.js
+++ atividade 07/index.js
@@ -27,23 +27,32 @@ lukas.calcAge(20)
 // Nesta atividade estou mostrando basicamente um dos motivos para não se utilizar var
 // O recomendável é não se utilizar arrow function para object methods
 
+// Um outro grande problema, não dá para utilizar o método "this." quando uma função está dentro da outra
 const z_Generation = {
 
-    year: 2006,
+    year: 2010,
     calcGeneration: function () {
 
         if (this.year >= 1997 && this.year <= 2009) {
             console.log("You're from generation Z");
         }
 
-        // Primeira solução, 
+        // Primeira solução, criar um variável 
         const self = this;
         const notZ = function () {
-            if (self.year < 1997 && self.year > 2009) {
-                console.log("Your are not from generation Z");
+            if (self.year < 1997 || self.year > 2009) {
+                console.log("You not are from generation Z");
             }
         }
         notZ();
+
+        // Segunda solução, apenas criar uma arrow function
+        const isMillenial = () => {
+            if (this.year >= 1981 && this.year <= 1996) {
+                console.log("You're from millenial generation!");
+            }
+        }
+        isMillenial();
     }
 
 }
