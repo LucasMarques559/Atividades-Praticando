2862756d9dbae79baa3bc5b0d20e909d89579691
Lucas Marques <lucasmarques180506@gmail.com>
2026-07-13 10:17:18 -0300

    feat: command codes

--- atividade 07/index.js
+++ atividade 07/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // variáveis globais declaradas com var utiliza-se o método "this.", portanto, é do próprio "window"
 var firstName = "Josh";
 // já variáveis como "let" e "const" não se usa o método "this." dentro das arrows functions
@@ -21,3 +23,29 @@ const lukas = {
 
 lukas.metro();
 lukas.calcAge(20)
+
+// Nesta atividade estou mostrando basicamente um dos motivos para não se utilizar var
+// O recomendável é não se utilizar arrow function para object methods
+
+const z_Generation = {
+
+    year: 2006,
+    calcGeneration: function () {
+
+        if (this.year >= 1997 && this.year <= 2009) {
+            console.log("You're from generation Z");
+        }
+
+        // Primeira solução, 
+        const self = this;
+        const notZ = function () {
+            if (self.year < 1997 && self.year > 2009) {
+                console.log("Your are not from generation Z");
+            }
+        }
+        notZ();
+    }
+
+}
+
+z_Generation.calcGeneration();
