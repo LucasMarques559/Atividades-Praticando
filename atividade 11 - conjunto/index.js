33e6682b1ac972767da4a3e241aa2cb38b3d0e8e
Lucas Marques <lucasmarques180506@gmail.com>
2026-08-03 12:35:02 -0300

    Métodos em conjuntos

--- atividade 11 - conjunto/index.js
+++ atividade 11 - conjunto/index.js
@@ -24,3 +24,39 @@ const funCozinha = ['Chefe', 'Chefe', 'Garçom', 'Garçom', 'Garçom', 'Manager'
 
 const funCozinhaSet = [...new Set(funCozinha)];
 console.log(funCozinhaSet);
+
+const italianFoods = new Set([
+    'Pasta',
+    'Gnocci',
+    'Tomatoes',
+    'Olive oil',
+    'Garlic',
+    'Basil'
+]);
+
+const mexicanFoods = new Set([
+    'Tortillas',
+    'Beans',
+    'Rice',
+    'Tomatoes',
+    'Avocado',
+    'Garlic'
+]);
+
+const commonFoods = italianFoods.intersection(mexicanFoods);
+console.log('Comidas em comum dos cardápios: ', [...commonFoods]);
+
+const italianMexicanUnion = italianFoods.union(mexicanFoods);
+console.log('A união dos cardápios: ', [...italianMexicanUnion]);
+// console.log(italianMexicanUnion);
+
+const differenceOfItalian = italianFoods.difference(mexicanFoods);
+console.log('A diferença dos cardápios: ', [...differenceOfItalian]);
+
+// Atenção: a ordem dos fatores altera o resultado!
+
+const differenceOfMexican = mexicanFoods.difference(italianFoods);
+console.log('Tem no mexicano mas não tem no italiano:', [...differenceOfMexican]);
+
+//symmetricDifference
+//isDisjoinFrom
