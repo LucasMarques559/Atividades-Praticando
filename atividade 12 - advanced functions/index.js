117929fc6e3e38597c9df8f4e4ae09be3c8d418a
Lucas Marques <lucasmarques180506@gmail.com>
2026-08-25 10:15:05 -0300

    challenge poll

--- atividade 12 - advanced functions/index.js
+++ atividade 12 - advanced functions/index.js
@@ -170,3 +170,20 @@ bookWE(29, 'seila');
 const bookWE23 = book.bind(eurowings, 23); // aqui eu defini que é sempre o número 23
 bookWE23('Lukas Marques');
 
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+    console.log(this);
+
+    this.planes++;
+    console.log(this.planes);
+}
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200))
+
+// O primeiro argumento do método bind é this
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
