0ae352d7067ec637da9cd75bc7974dcbc6aa6e9f
Lucas Marques <lucasmarques180506@gmail.com>
2026-08-26 12:10:11 -0300

    arrays methods

--- projeto 1.2 - functions/script.js
+++ projeto 1.2 - functions/script.js
@@ -48,3 +48,23 @@ const poll = {
 document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
 poll.displayResults.call({ answers: [5, 2, 3] });
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+
+// This is more of a thinking challenge than a coding challenge 🤓
+
+// Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+// And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+// GOOD LUCK 😀
+// */
+
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.body.addEventListener('click', function () {
+        header.style.color = 'blue';
+    });
+
+})();
